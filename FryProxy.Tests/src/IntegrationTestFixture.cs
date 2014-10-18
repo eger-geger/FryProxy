490a0afc5bab4c11e9f6b1d0cb5ae5b231b88c50
ivan.mihaylovskyy <ivan.mihaylovskyy@pearl.com>
2014-10-18 14:29:18 +0300

    adding tests

---
+++ FryProxy.Tests/src/IntegrationTestFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+
+namespace FryProxy.Tests {
+
+    public class IntegrationTestFixture {
+
+        protected IWebDriver WebDriver { get; private set; }
+
+        public void SetUpBrowserProxy() {
+            WebDriver = new FirefoxDriver(
+                new DesiredCapabilities(
+                    new Dictionary<String, Object> {
+                        {
+                            CapabilityType.Proxy, new Proxy {
+                                HttpProxy = "",
+                                SslProxy = "",
+                                Kind = ProxyKind.Manual
+                            }
+                        }
+                    }));
+        }
+
+        public void ShutdownBrowserAndProxy() {}
+
+    }
+
+}
