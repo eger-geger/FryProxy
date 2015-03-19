af57efeb82a8f5f6bd6c9c5a230b651f14058bb9
Ivan Mihaylovskyy <edongrossi1@gmail.com>
2015-03-19 22:58:42 +0200

    Added FryProxy.ConsoleApp

---
+++ FryProxy.ConsoleApp/src/CommandlineOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CommandLine;
+using CommandLine.Text;
+
+namespace FryProxy.ConsoleApp
+{
+    public class CommandlineOptions {
+
+        [Option('h', "host", DefaultValue ="localhost", HelpText = "Hostname on to listen")]
+        public String Host { get; set; }
+
+        [HelpOption]
+        public String GetUsage()
+        {
+            return HelpText.AutoBuild(this);
+        }
+    }
+}
---
+++ FryProxy.ConsoleApp/src/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using CommandLine;
+
+namespace FryProxy.ConsoleApp
+{
+    public class Program
+    {
+        private const String CertificateFileName = "fry.pfx";
+
+        private const String CertificatePassword = "fry";
+
+        private const String StopCommad = "stop";
+
+        private static X509Certificate2 Certificate
+        {
+            get
+            {
+                Stream certificateStream = Assembly.GetExecutingAssembly()
+                    .GetManifestResourceStream(typeof (Program), CertificateFileName);
+
+                if (certificateStream == null)
+                {
+                    throw new InvalidOperationException("Failed to load SSL certificate");
+                }
+
+                var certificateBytes = new byte[certificateStream.Length];
+
+                certificateStream.Read(certificateBytes, 0, certificateBytes.Length);
+
+                return new X509Certificate2(certificateBytes, CertificatePassword);
+            }
+        }
+
+        public static void Main(String[] args)
+        {
+            var options = new CommandlineOptions();
+
+            if (!Parser.Default.ParseArguments(args, options))
+            {
+                return;
+            }
+
+            try
+            {
+                var httpProxyServer = new HttpProxyServer(options.Host, new HttpProxy());
+
+                var sslProxyServer = new HttpProxyServer(options.Host, new SslProxy(Certificate));
+
+                WaitHandle.WaitAll(new[]
+                {
+                    httpProxyServer.Start(),
+                    sslProxyServer.Start()
+                });
+
+                Console.WriteLine("Started HTTP proxy on {0}", httpProxyServer.ProxyEndPoint);
+                Console.WriteLine("Started SSL proxy on {0}", sslProxyServer.ProxyEndPoint);
+                Console.WriteLine("Type 'stop' into console in order to stop");
+
+                while (Console.ReadLine() != StopCommad)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                }
+
+                Console.WriteLine("shutting down...");
+
+                httpProxyServer.Stop();
+                sslProxyServer.Stop();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
