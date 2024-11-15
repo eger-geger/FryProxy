0b80c189c3b1a4504db9c556e735f2aca08c864d
Ivan Mihaylovskyy <ivan.mihaylovskyy@gmail.com>
2024-11-15 09:13:31 +0100

    Hpack numeric binary codec

---
+++ FryProxy.Tests/Http/Hpack/CodecTests.fs
@@ -0,0 +1,36 @@
+﻿module FryProxy.Tests.Http.Hpack.CodecTests
+
+open System
+open FryProxy.Http.Hpack
+open NUnit.Framework
+open FsUnit
+
+[<TestCase(3, 10UL, ExpectedResult = "1010")>]
+[<TestCase(0, 42UL, ExpectedResult = "101010")>]
+[<TestCase(3, 1337UL, ExpectedResult = "11111|10011010|1010")>]
+let testEncodeNumber (offset, n) =
+    (Codec.encodeInt offset n).ToArray()
+    |> Array.map(sprintf "%B")
+    |> Array.reduce(sprintf "%s|%s")
+
+[<TestCase(3, "1010", ExpectedResult = 10UL)>]
+[<TestCase(0, "101010", ExpectedResult = 42UL)>]
+[<TestCase(3, "11111|10011010|1010", ExpectedResult = 1337UL)>]
+let testDecodeNumber (offset, bytes: string) =
+    bytes.Split('|')
+    |> Array.map(fun s -> Convert.ToByte(s, 2))
+    |> Codec.decodeInt offset
+
+[<Test>]
+let testDecodeNumberOverflow () =
+    let bytes = Array.append <| Array.create 10 255uy <| [| 15uy |]
+
+    (fun () -> Codec.decodeInt 0 bytes |> ignore)
+    |> should throw typeof<OverflowException>
+
+[<Test>]
+let testDecodeNumberIncompleteSequence () =
+    let bytes = [| 255uy; 255uy |]
+
+    (fun () -> Codec.decodeInt 0 bytes |> ignore)
+    |> should throw typeof<ArgumentException>
---
+++ FryProxy/Http/Hpack/Codec.fs
@@ -0,0 +1,52 @@
+﻿module FryProxy.Http.Hpack.Codec
+
+open System
+
+let inline byteCap offset = 255uy >>> offset
+
+let encodeInt offset n =
+    let rec loop n acc =
+        if n < 128UL then
+            byte n :: acc |> List.rev
+        else
+            (128uy ||| byte(n % 128UL)) :: acc |> loop(n / 128UL)
+
+    let bytes =
+        let cap = byteCap offset |> uint64
+
+        if n < cap then
+            [| byte n |]
+        else
+            [ byte cap ] |> loop(n - cap) |> List.toArray
+
+    Span(bytes)
+
+let decodeInt offset (bytes: byte seq) =
+    let cap = byteCap offset
+    use is = (Seq.indexed bytes).GetEnumerator()
+
+    let inline next () =
+        if is.MoveNext() then
+            is.Current
+        else
+            invalidArg (nameof bytes) "incomplete byte sequence"
+
+    let rec loop size acc =
+        let i, n = next()
+        let carry = n &&& 128uy
+        let acc' = acc + (uint64(n ^^^ carry) <<< 7 * (i - 1))
+
+        if size > 64 then
+            raise(OverflowException $"number size exceeds uint64: {size}")
+        elif carry = 0uy then
+            acc'
+        else
+            acc' |> loop(size + 7)
+
+    let _, first = next()
+    let prefix = cap &&& first
+
+    if prefix < cap then
+        uint64 prefix
+    else
+        uint64 cap |> loop(8 - offset)
