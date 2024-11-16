961529fcd9046933e61a5d8d2ff5b66c05c1e85c
Ivan Mihaylovskyy <ivan.mihaylovskyy@gmail.com>
2024-11-16 21:30:34 +0100

    Introduce decoder monad and refactor numeric representation into its own module

--- FryProxy.Tests/Http/Hpack/CodecTests.fs
+++ FryProxy.Tests/Http/Hpack/CodecTests.fs
@@ -9,7 +9,8 @@ open FsUnit
 [<TestCase(0, 42UL, ExpectedResult = "101010")>]
 [<TestCase(3, 1337UL, ExpectedResult = "11111|10011010|1010")>]
 let testEncodeNumber (offset, n) =
-    (Codec.encodeInt offset n).ToArray()
+    NumericLit.from64 offset n
+    |> NumericLit.toArray
     |> Array.map(sprintf "%B")
     |> Array.reduce(sprintf "%s|%s")
 
@@ -20,19 +21,13 @@ let testEncodeNumber (offset, n) =
 let testDecodeNumber (offset, bytes: string) =
     bytes.Split('|')
     |> Array.map(fun s -> Convert.ToByte(s, 2))
-    |> Codec.decodeInt offset
-    |> Result.defaultValue 0UL
-
-[<Test>]
-let testDecodeNumberOverflow () =
-    let bytes = Array.append <| Array.create 10 255uy <| [| 15uy |]
-
-    Codec.decodeInt 0 bytes
-    |> should be (ofCase <@ Result<uint64, string>.Error "" @>)
+    |> NumericLit.decode offset 0
+    |> Decoder.defaultValue NumericLit.zero
+    |> NumericLit.to64
 
 [<Test>]
 let testDecodeNumberIncompleteSequence () =
     let bytes = [| 255uy; 255uy |]
 
-    Codec.decodeInt 0 bytes
-    |> should be (ofCase <@ Result<uint64, string>.Error "" @>)
+    NumericLit.decode 0 0 bytes
+    |> should be (ofCase <@ DecodeResult<NumericLit>.DecErr("", 0) @>)
---
+++ FryProxy/Http/Hpack/Numeric.fs
@@ -0,0 +1,79 @@
+﻿namespace FryProxy.Http.Hpack
+
+open System
+open Microsoft.FSharp.Core
+
+/// Unsigned number binary representation.
+[<Struct>]
+type NumericLit = { Prefix: uint8; MSB: uint8 list }
+
+module NumericLit =
+
+    let zero = { Prefix = 0uy; MSB = List.Empty }
+
+    let inline byteCap offset = 255uy >>> offset
+
+    /// Encode numeric value into octet sequence skipping given number of bits within first octet.
+    let inline from ntp offset (n: ^a) =
+        let n128 = ntp 128uy
+
+        let rec loop m acc =
+            if m < n128 then
+                byte m :: acc
+            else
+                (128uy ||| byte(m % n128)) :: acc |> loop(m / n128)
+
+        let cap = byteCap offset
+
+        if n < ntp cap then
+            { Prefix = byte n; MSB = List.Empty }
+        else
+            { Prefix = cap; MSB = [] |> loop(n - (ntp cap)) }
+
+    let from8 = from uint8
+
+    let from16 = from uint16
+
+    let from32 = from uint32
+
+    let from64 = from uint64
+
+    let toList { Prefix = p; MSB = msb } = p :: (List.rev msb)
+
+    let toArray = toList >> List.toArray
+
+    let toSpan n = Span(toArray n)
+
+    let to64 { Prefix = p; MSB = msb } =
+        msb
+        |> List.rev
+        |> List.indexed
+        |> List.fold (fun acc (i, b) -> acc + (uint64(b &&& 127uy) <<< 7 * i)) (uint64 p)
+
+
+    [<TailCall>]
+    let rec private decodeMSB acc j bs =
+        match Decoder.take j bs with
+        | DecVal(b, off) ->
+            let acc' = b :: acc
+
+            if b &&& 128uy = 0uy then
+                DecVal(acc', off)
+            else
+                (off, bs) ||> decodeMSB acc'
+        | DecErr(msg, off) -> DecErr(msg, off)
+
+    /// Decode numeric value from octet sequence ignoring given number of bits in first octet.
+    let decode offset =
+        let cap = byteCap offset
+
+        decoder {
+            let! b = Decoder.take
+            let prefix = cap &&& b
+
+            if prefix < cap then
+                return { Prefix = prefix; MSB = List.Empty }
+            else
+                let! msb = decodeMSB List.Empty
+                return { Prefix = prefix; MSB = msb }
+        }
