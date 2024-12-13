b6b9e362097652ae6e21f64997b5148861f380b2
Ivan Mihaylovskyy <ivan.mihaylovskyy@gmail.com>
2024-12-13 22:32:13 +0100

    Encode numeric value to stack-allocated array

---
+++ FryProxy/Extension/Stackalloc.fs
@@ -0,0 +1,11 @@
+﻿module FryProxy.Extension.Stackalloc
+
+#nowarn "9"
+
+open System
+open FSharp.NativeInterop
+
+/// Allocate array on the stack.
+let inline span length =
+    let p = NativePtr.stackalloc<'a> length |> NativePtr.toVoidPtr
+    Span<'a>(p, length)
