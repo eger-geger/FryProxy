136217df9d9fd6b06b121c5ffb2c7e03f396d1aa
Ivan Mihaylovskyy <ivan.mihaylovskyy@gmail.com>
2024-12-14 08:50:01 +0100

    Encode raw string literal

--- FryProxy/Extension/Stackalloc.fs
+++ FryProxy/Extension/Stackalloc.fs
@@ -5,7 +5,17 @@
 open System
 open FSharp.NativeInterop
 
+[<Literal>]
+let MediumThreshold = 256
+
 /// Allocate array on the stack.
 let inline span length =
     let p = NativePtr.stackalloc<'a> length |> NativePtr.toVoidPtr
     Span<'a>(p, length)
+
+/// Allocate array on the stack or heap, depending on size.
+let inline medium length =
+    if length < MediumThreshold then
+        span length
+    else
+        Span(Array.zeroCreate length)
