af1e45dc968ad7b49853de9712545a22a2b0827d
Ivan Mihaylovskyy <ivan.mihaylovskyy@gmail.com>
2024-12-22 16:03:01 +0100

    Finalize octet encoding and decoding

--- FryProxy.Tests/Http/Hpack/CommandTests.fs
+++ FryProxy.Tests/Http/Hpack/CommandTests.fs
@@ -3,6 +3,9 @@
 open NUnit.Framework
 open FryProxy.Http.Hpack
 
+let flipTestCase (tc: TestCaseData) =
+    TestCaseData(tc.ExpectedResult).Returns(tc.Arguments[0]).SetName(tc.TestName)
+
 let decodeCommandTestCases =
     [ TestCaseData("82")
           .SetName("C.2.4 Indexed Header Field")
@@ -10,15 +13,16 @@ let decodeCommandTestCases =
 
       TestCaseData("400a 6375 7374 6f6d 2d6b 6579 0d63 7573 746f 6d2d 6865 6164 6572")
           .SetName("C.2.1 Literal Header Field with Indexing")
-          .Returns(IndexedLiteralField { Name = Literal "custom-key"; Value = "custom-header" })
+          .Returns(IndexedLiteralField { Name = Literal(Raw "custom-key"); Value = Raw "custom-header" })
 
       TestCaseData("040c 2f73 616d 706c 652f 7061 7468")
           .SetName("C.2.2 Literal Header Field without Indexing")
-          .Returns(NonIndexedLiteralField { Name = Indexed 4us; Value = "/sample/path" })
+          .Returns(NonIndexedLiteralField { Name = Indexed 4us; Value = Raw "/sample/path" })
 
       TestCaseData("1008 7061 7373 776f 7264 0673 6563 7265 74")
           .SetName("C.2.3 Literal Header Field Never Indexed")
-          .Returns(NeverIndexedLiteralField { Name = Literal "password"; Value = "secret" }) ]
+          .Returns(NeverIndexedLiteralField { Name = Literal(Raw "password"); Value = Raw "secret" }) ]
+
 
 [<TestCaseSource(nameof decodeCommandTestCases)>]
 let testDecodeCommand (hex: string) =
@@ -26,66 +30,70 @@ let testDecodeCommand (hex: string) =
     | Ok cmd, _ -> cmd
     | Error err, _ -> failwith err
 
-let encodeCommandTestCases =
-    decodeCommandTestCases
-    |> List.map(fun tc -> TestCaseData(tc.ExpectedResult).Returns(tc.Arguments[0]).SetName(tc.TestName))
+let encodeCommandTestCases = decodeCommandTestCases |> List.map flipTestCase
 
 [<TestCaseSource(nameof encodeCommandTestCases)>]
 let testEncodeCommand (cmd: Command) =
     Hex.OctetWriter(Command.encodeCommand cmd) |> Hex.runWriter
 
-let blockTestCases =
-    let firstRequest =
+let decodeBlockTestCases =
+    let firstRequest ctor =
         [ IndexedField(2us)
           IndexedField(6us)
           IndexedField(4us)
-          IndexedLiteralField({ Name = Indexed(1us); Value = "www.example.com" }) ]
+          IndexedLiteralField({ Name = Indexed(1us); Value = ctor "www.example.com" }) ]
 
-    let secondRequest =
+    let secondRequest ctor =
         [ IndexedField(2us)
           IndexedField(6us)
           IndexedField(4us)
           IndexedField(62us)
-          IndexedLiteralField({ Name = Indexed(24us); Value = "no-cache" }) ]
+          IndexedLiteralField({ Name = Indexed(24us); Value = ctor "no-cache" }) ]
 
-    let thirdRequest =
+    let thirdRequest ctor =
         [ IndexedField(2us)
           IndexedField(7us)
           IndexedField(5us)
           IndexedField(63us)
-          IndexedLiteralField({ Name = Literal("custom-key"); Value = "custom-value" }) ]
+          IndexedLiteralField({ Name = Literal(ctor "custom-key"); Value = ctor "custom-value" }) ]
 
     [ TestCaseData("", TestName = "Empty", ExpectedResult = List.empty<Command>)
 
       TestCaseData("8286 8441 0f77 7777 2e65 7861 6d70 6c65 2e63 6f6d")
           .SetName("C.3.1 First Request")
-          .Returns(firstRequest)
+          .Returns(firstRequest Raw)
 
       TestCaseData("8286 8441 8cf1 e3c2 e5f2 3a6b a0ab 90f4 ff")
           .SetName("C.4.1 First Request")
           .SetCategory("Huffman")
-          .Returns(firstRequest)
+          .Returns(firstRequest Huf)
 
       TestCaseData("8286 84be 5808 6e6f 2d63 6163 6865")
           .SetName("C.3.2 Second Request")
-          .Returns(secondRequest)
+          .Returns(secondRequest Raw)
 
       TestCaseData("8286 84be 5886 a8eb 1064 9cbf")
           .SetName("C.4.2 Second Request")
           .SetCategory("Huffman")
-          .Returns(secondRequest)
+          .Returns(secondRequest Huf)
 
       TestCaseData("8287 85bf 400a 6375 7374 6f6d 2d6b 6579 0c63 7573 746f 6d2d 7661 6c75 65")
           .SetName("C.3.3 Third Request")
-          .Returns(thirdRequest)
+          .Returns(thirdRequest Raw)
 
       TestCaseData("8287 85bf 4088 25a8 49e9 5ba9 7d7f 8925 a849 e95b b8e8 b4bf")
           .SetName("C.4.3 Third Request")
           .SetCategory("Huffman")
-          .Returns(thirdRequest) ]
+          .Returns(thirdRequest Huf) ]
 
-[<TestCaseSource(nameof blockTestCases)>]
+[<TestCaseSource(nameof decodeBlockTestCases)>]
 let testDecodeBlock (hex: string) =
     match Hex.decodeArr hex |> Decoder.runArr Command.decodeBlock with
     | Ok commands -> commands
     | Error err -> failwith err
+
+let encodeBlockTestCases = decodeBlockTestCases |> List.map flipTestCase
+
+[<TestCaseSource(nameof encodeBlockTestCases)>]
+let testEncodeBlock block =
+    Hex.OctetWriter(Command.encodeBlock block) |> Hex.runWriter
--- FryProxy/Http/Hpack/String.fs
+++ FryProxy/Http/Hpack/String.fs
@@ -1,61 +1,76 @@
-﻿module FryProxy.Http.Hpack.StringLit
+﻿namespace FryProxy.Http.Hpack
 
 open System
 open System.Text
 open Microsoft.FSharp.Core
 
-[<Literal>]
-let HuffmanEncodedFlag = 0b1000_0000uy
+/// Raw or Huffman-coded string literal.
+[<Struct>]
+type StringLit =
+    | Raw of string
+    | Huf of string
 
-[<Literal>]
-let MaxLength = 0xffff
+module StringLit =
 
-/// Encode ASCII string as raw literal.
-let inline encodeRaw (str: string) (buf: byte Span) =
-    let len = NumericLit.encode 1 (uint64 str.Length) buf
-    Encoding.ASCII.GetBytes(str, buf.Slice(len)) + len
+    [<Literal>]
+    let HuffmanEncodedFlag = 0b1000_0000uy
 
-/// Encode extended ASCII string using static Huffman table.
-let inline encodeHuf (str: string) (buf: byte Span) =
-    let sLen = Huffman.encodeStr str (buf.Slice(9))
-    let nLen = NumericLit.encode 1 (uint64 sLen) buf
-    let total = sLen + nLen
-    
-    buf.Slice(9, sLen).CopyTo(buf.Slice(nLen))
-    buf[0] <- buf[0] ||| HuffmanEncodedFlag
-    buf.Slice(total, 9 - nLen).Clear()
-    total
+    [<Literal>]
+    let MaxLength = 0xffff
 
-/// Decode ASCII string of given length.
-let inline decodeRaw len =
-    decoder {
-        let! arr = Decoder.takeN len
-        return String([| for c in arr -> char c |])
-    }
+    let inline toString lit =
+        match lit with
+        | Raw str -> str
+        | Huf str -> str
 
-/// Decode Huffman byte sequence of given length.
-let inline decodeHuf len =
-    decoder {
-        let! arr = Decoder.takeN len
+    /// Encode ASCII string as raw literal.
+    let inline encodeRaw (str: string) (buf: byte Span) =
+        let len = NumericLit.encode 1 (uint64 str.Length) buf
+        Encoding.ASCII.GetBytes(str, buf.Slice(len)) + len
 
-        match Huffman.decodeStr arr with
-        | Ok s -> return s
-        | Error e -> return! Decoder.error e
-    }
+    /// Encode extended ASCII string using static Huffman code.
+    let inline encodeHuf (str: string) (buf: byte Span) =
+        let sLen = Huffman.encodeStr str (buf.Slice(9))
+        let nLen = NumericLit.encode 1 (uint64 sLen) buf
+        let total = sLen + nLen
 
-/// Decode ASCII string literal.
-let decode =
-    decoder {
-        let! first = Decoder.peek
-        let! lenLit = NumericLit.decode 1
+        buf.Slice(9, sLen).CopyTo(buf.Slice(nLen))
+        buf[0] <- buf[0] ||| HuffmanEncodedFlag
+        buf.Slice(total, 9 - nLen).Clear()
+        total
 
-        let decoder =
-            if first |> Flag.check HuffmanEncodedFlag then
-                decodeHuf
-            else
-                decodeRaw
+    /// Encode ASCII string to octets optionally using Huffman code.
+    let inline encode lit buf =
+        match lit with
+        | Raw str -> encodeRaw str buf
+        | Huf str -> encodeHuf str buf
 
-        match NumericLit.toUint16 lenLit with
-        | Ok len -> return! decoder len
-        | Error msg -> return! Decoder.error msg
-    }
+    /// Decode ASCII string of given length.
+    let inline decodeRaw len =
+        decoder {
+            let! arr = Decoder.takeN len
+            return Raw(String([| for c in arr -> char c |]))
+        }
+
+    /// Decode Huffman byte sequence of given length.
+    let inline decodeHuf len =
+        decoder {
+            let! arr = Decoder.takeN len
+
+            match Huffman.decodeStr arr with
+            | Ok s -> return Huf s
+            | Error e -> return! Decoder.error e
+        }
+
+    /// Decode ASCII string literal and determine whether it was encoded with Huffman code.
+    let decode =
+        decoder {
+            let! first = Decoder.peek
+            let! lenLit = NumericLit.decode 1
+            let huffman = first |> Flag.check HuffmanEncodedFlag
+            let decoder = if huffman then decodeHuf else decodeRaw
+
+            match NumericLit.toUint16 lenLit with
+            | Ok len -> return! decoder len
+            | Error msg -> return! Decoder.error msg
+        }
