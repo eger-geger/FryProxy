446e4653d3fe655aa137ae3cc214bada09409184
Ivan Mihaylovskyy <ivan.mihaylovskyy@gmail.com>
2024-12-23 13:38:51 +0100

    Simple field decoding involving dynamic table

--- FryProxy/Http/Hpack/Table.fs
+++ FryProxy/Http/Hpack/Table.fs
@@ -1,28 +1,29 @@
 ﻿module FryProxy.Http.Hpack.Table
 
+open System
 open FryProxy.Http
 
 type Entry = { Field: Field; Size: uint64 }
 
 type DynamicTable = { Entries: Entry List; SizeLimit: uint64 }
 
-let emptyTable = { Entries = List.empty; SizeLimit = 0UL }
+let empty = { Entries = List.empty; SizeLimit = 0UL }
 
 let staticTable =
-    [ { Name = ":authority"; Values = List.Empty }
-      { Name = ":method"; Values = [ "GET" ] }
-      { Name = ":method"; Values = [ "POST" ] }
-      { Name = ":path"; Values = [ "/" ] }
-      { Name = ":path"; Values = [ "/index.html" ] }
-      { Name = ":scheme"; Values = [ "http" ] }
-      { Name = ":scheme"; Values = [ "https" ] }
-      { Name = ":status"; Values = [ "200" ] }
-      { Name = ":status"; Values = [ "204" ] }
-      { Name = ":status"; Values = [ "206" ] }
-      { Name = ":status"; Values = [ "304" ] }
-      { Name = ":status"; Values = [ "400" ] }
-      { Name = ":status"; Values = [ "404" ] }
-      { Name = ":status"; Values = [ "500" ] }
+    [ { Name = ":authority:"; Values = List.Empty }
+      { Name = ":method:"; Values = [ "GET" ] }
+      { Name = ":method:"; Values = [ "POST" ] }
+      { Name = ":path:"; Values = [ "/" ] }
+      { Name = ":path:"; Values = [ "/index.html" ] }
+      { Name = ":scheme:"; Values = [ "http" ] }
+      { Name = ":scheme:"; Values = [ "https" ] }
+      { Name = ":status:"; Values = [ "200" ] }
+      { Name = ":status:"; Values = [ "204" ] }
+      { Name = ":status:"; Values = [ "206" ] }
+      { Name = ":status:"; Values = [ "304" ] }
+      { Name = ":status:"; Values = [ "400" ] }
+      { Name = ":status:"; Values = [ "404" ] }
+      { Name = ":status:"; Values = [ "500" ] }
       { Name = "accept-charset"; Values = List.Empty }
       { Name = "accept-encoding"; Values = [ "gzip"; "deflate" ] }
       { Name = "accept-language"; Values = List.Empty }
@@ -72,10 +73,13 @@ let staticTable =
       { Name = "www-authenticate"; Values = List.Empty } ]
 
 let tryItem i (table: DynamicTable) =
-    if i < staticTable.Length then
-        ValueSome staticTable[i]
-    elif i < table.Entries.Length then
-        ValueSome table.Entries[i].Field
+    let si = i - 1
+    let di = si - staticTable.Length
+
+    if si < staticTable.Length then
+        ValueSome staticTable[si]
+    elif di < table.Entries.Length then
+        ValueSome table.Entries[di].Field
     else
         ValueNone
 
@@ -108,3 +112,57 @@ let push (e: Entry) (tbl: DynamicTable) =
         { tbl with Entries = List.empty }
     else
         { tbl with Entries = e :: tbl.Entries } |> resize tbl.SizeLimit
+
+let inline private entryNotFoundError idx = Error $"field at {idx} does not exist"
+
+let inline private indexedFieldName idx table =
+    match tryItem (int idx) table with
+    | ValueNone -> entryNotFoundError idx
+    | ValueSome field -> Ok field.Name
+
+let inline private resolveLiteralFieldName name table =
+    match name with
+    | Indexed idx -> indexedFieldName idx table
+    | Literal lit -> StringLit.toString lit |> Ok
+
+let inline private literalValueField name litVal =
+    { Name = name; Values = litVal |> StringLit.toString |> Field.decodeValues }
+
+let inline private addIndexedField table fields litVal name =
+    let fld = literalValueField name litVal
+    fld :: fields, table |> push(entry fld)
+
+let inline private addNonIndexedField table fields litVal name =
+    let fld = literalValueField name litVal
+    fld :: fields, table
+
+let inline runCommand (fields: Field List, table: DynamicTable) cmd : Result<Field List * DynamicTable, string> =
+    match cmd with
+    | TableSize size -> Ok(fields, resize (uint64 size) table)
+    | IndexedField idx ->
+        match tryItem (int idx) table with
+        | ValueNone -> entryNotFoundError idx
+        | ValueSome field -> Ok(field :: fields, table)
+    | IndexedLiteralField field ->
+        table
+        |> resolveLiteralFieldName field.Name
+        |> Result.map(addIndexedField table fields field.Value)
+    | NonIndexedLiteralField field ->
+        table
+        |> resolveLiteralFieldName field.Name
+        |> Result.map(addNonIndexedField table fields field.Value)
+    | NeverIndexedLiteralField field ->
+        table
+        |> resolveLiteralFieldName field.Name
+        |> Result.map(addNonIndexedField table fields field.Value)
+
+[<TailCall>]
+let rec runCommandBlock block (fields: Field List, table: DynamicTable) =
+    match block with
+    | [] -> Ok(fields, table)
+    | head :: tail -> runCommand (fields, table) head |> Result.bind(runCommandBlock tail)
+
+let decodeFields (table: DynamicTable) (octets: byte ReadOnlySpan) : Result<Field List * DynamicTable, string> =
+    match Decoder.run Command.decodeBlock octets with
+    | Ok commands -> runCommandBlock commands ([], table)
+    | Error err -> Error err
