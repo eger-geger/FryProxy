ab8e58883b67feb1985f1d2a269b519fd9c697b4
Ivan Mihaylovskyy <ivan.mihaylovskyy@gmail.com>
2025-02-08 22:53:59 +0100

    Basic FrameHeader type and tests

---
+++ FryProxy.Tests/Http/Frames/FrameHeaderTests.fs
@@ -0,0 +1,17 @@
+﻿module FryProxy.Tests.Http.Frames.FrameHeaderTests
+
+open System
+open System.Buffers
+open FryProxy.Http.Frames
+open FsUnit
+open NUnit.Framework
+
+[<Test>]
+let testEncodeDecode () =
+    use memOwner = MemoryPool.Shared.Rent()
+
+    let fh = { Length = 648u; Flags = 3uy; Type = 4uy; StreamId = 45u }
+
+    FrameHeader.encode fh memOwner.Memory.Span |> should equal 9
+
+    FrameHeader.decode(memOwner.Memory.Span.Slice(0, 9)) |> should equal fh
---
+++ FryProxy/Http/Frames/FrameHeader.fs
@@ -0,0 +1,52 @@
+﻿namespace FryProxy.Http.Frames
+
+open System
+
+[<Struct>]
+type FrameHeader =
+    { Length: uint32 // =< 24 bits
+      Type: uint8 // TODO: make enum
+      Flags: uint8 // type specific flags
+      StreamId: uint32 } // =< 31 bits
+
+module FrameHeader =
+
+    [<Literal>]
+    let StreamIdMask = 0x7fffffffu
+
+    let rec private decodeNum (buf: byte ReadOnlySpan) n acc =
+        if n = 0 then
+            acc
+        else
+            let acc' = uint32(buf[0]) + (acc <<< 8)
+            decodeNum (buf.Slice(1)) (n - 1) acc'
+
+    let rec private encodeNum (buf: byte Span) num len =
+        if len = 0 then
+            ()
+        else
+            buf[(len - 1)] <- byte num
+            encodeNum buf (num >>> 8) (len - 1)
+
+    let decode (buf: byte ReadOnlySpan) =
+        if buf.Length <> 9 then
+            invalidArg (nameof(buf)) $"invalid header buffer size: {buf.Length}"
+
+        { Length = decodeNum buf 3 0u
+          Type = buf[3]
+          Flags = buf[4]
+          StreamId = StreamIdMask &&& decodeNum (buf.Slice(5)) 4 0u }
+
+
+    let encode (fh: FrameHeader) (buf: byte Span) =
+        if fh.Length > 0xffffffu then
+            invalidArg (nameof fh) $"length exceeds 24 bits: {fh.Length}"
+
+        if fh.StreamId > StreamIdMask then
+            invalidArg (nameof fh) $"stream identifier exceeds 31 bits: {fh.StreamId}"
+
+        do encodeNum buf fh.Length 3
+        do buf[3] <- fh.Type
+        do buf[4] <- fh.Flags
+        do encodeNum (buf.Slice(5)) fh.StreamId 4
+        9
